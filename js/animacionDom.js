eac5f531a215fbaa79e6233259b08752f67ea32d
mattyws-dev <owen.anguloc@gmail.com>
2025-01-18 23:52:29 -0500

    animaciones para tareas completadas y para eliminar tarea: completo

--- js/animacionDom.js
+++ js/animacionDom.js
@@ -10,3 +10,29 @@ document.querySelector("#btnMenu").addEventListener("click", (e)=>{
         }
       });
 })
+
+document.querySelector(".lista-tareas").addEventListener('click', (e)=>{
+  e.preventDefault()
+  if(e.target.localName === "i"){
+
+    const tarea = document.querySelector(`#tarea${e.target.id}`)  
+  
+    if (e.target.classList[1] === "fa-check") {
+      tarea.classList.add("tarea-completada")
+      
+      tarea.addEventListener('transitionend', (e) => {    
+        tarea.children[1].children[0].style.display = "none"
+      }, { once: true }); // se hace una sola vez
+          
+    } else {
+      tarea.classList.add("tarea-eliminada")
+  
+      tarea.addEventListener('transitionend', (e) => {    
+        tarea.style.display = "none"
+      }, { once: true }); // se hace una sola vez
+    }
+    
+  }
+  
+
+})
